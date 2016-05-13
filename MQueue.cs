ea989d756a5feda85f4ffd16c0ffa0090c5b1018
Paulo <paulo.santanna89@gmail.com>
2016-05-13 12:02:06 -0300

    Second Upload

---
+++ MQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Algorithms
+{
+    class MQueue
+    {
+        private ArrayList myQueue;
+
+        public MQueue()
+        {
+            myQueue = new ArrayList();
+        }
+
+        public void Enqueue(Object item)
+        {
+            myQueue.Add(item);
+        }
+
+        public void Dequeu()
+        {
+            myQueue.RemoveAt(0);
+        }
+
+        public object Peek()
+        {
+            return myQueue[0];
+        }
+
+        public void ClearQueue()
+        {
+            myQueue.Clear();
+        }
+
+        public int Count()
+        {
+            return myQueue.Count;
+        }
+
+    }
+}
---
+++ MStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Algorithms
+{
+    class MStack
+    {
+        public int top_index;
+        private ArrayList list;
+
+        public MStack()
+        {
+            list = new ArrayList();
+            top_index = -1;
+        }
+
+        public int count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public void push(Object item)
+        {
+            list.Add(item);
+            top_index++;
+        }
+
+        public object pop()
+        {
+            object obj = list[top_index];
+            list.RemoveAt(top_index);
+            top_index--;
+            return obj;
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+            top_index = -1;
+        }
+
+        public object peek()
+        {
+            return list[top_index];
+        }
+    }
+}
