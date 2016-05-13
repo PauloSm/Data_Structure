787c65b41918e5718eed142c8559597213734eae
Paulo <paulo.santanna89@gmail.com>
2016-05-13 12:49:45 -0300

    Second Upload

--- MDictionary.cs
+++ MDictionary.cs
@@ -9,22 +9,22 @@ namespace DicitionaryImplementation
 {
     struct Element<T> //Implement Size
     {
-        public int key;
-        public T value;
-        public int collision;
-        public ArrayList next;
+        public int key; // O elemento que será usado como chave. Precisa ser do tipo Int para a fuc. Hash
+        public T value; // Valor de tipo genérico 
+        public int collision; // Armazena o numero de colisões  
+        public ArrayList next; // Armazena a posição do elemento que colidiu 
     }
     class MDictionary <T>
     {
         Element<T>[] arr_items;
-        int[] arr_index;
+        int[] arr_index; // Array usado para armazenar os index's gerados
         int[] arr_keys;
         int index;
         int pointer;
-        int size;
+        int size; // Tamanho do Array
         Element<T> item;
-        ArrayList empty_position;
-        ArrayList primes;
+        ArrayList empty_position; // Armazena as posições vazias no array de index
+        ArrayList primes; // armazena os números primos que serão usados na função hash
 
         public MDictionary()
         {
@@ -37,7 +37,7 @@ namespace DicitionaryImplementation
             pointer = 0;
             for (int i = 0; i < arr_index.Length; i++)
             {
-                arr_index[i] = -1;
+                arr_index[i] = -1; //Array inicializado com -1 para facil identificação das pos. livres
                 empty_position.Add(i);
                 
             }
@@ -45,7 +45,7 @@ namespace DicitionaryImplementation
         
         public int Hash(int key)
         {
-            return key % size;
+            return key % size; // Modulo da Chave (Int) com o Primeiro primo da lista de primos.
         }
         public void Insert (int key, T value)
         {
@@ -57,7 +57,7 @@ namespace DicitionaryImplementation
             {
                 index = Hash(key);
                 
-                if (arr_index.Contains(index)) 
+                if (arr_index.Contains(index)) // houve colisão
                 {
                     arr_items[index].collision += 1;
                     arr_items[index].next = new ArrayList();
@@ -82,19 +82,19 @@ namespace DicitionaryImplementation
                     
                 }
                 
-                if (arr_index[arr_index.Length-1] != -1)
+                if (arr_index[arr_index.Length-1] != -1) // Se o array está lotado deve ser redimensionado 
                     Resize();
             }
         }
 
-        private int Collision()
+        private int Collision() // Verifica se ouve colisão nos index's gerados pela Func Hash
         {
             int index = (int)empty_position[0];
             empty_position.RemoveAt(0);
             return index;
         }
 
-        private bool DuplicateKey(int key)
+        private bool DuplicateKey(int key) // Verifica se há chaves duplicadas
         {
             if (arr_keys.Contains(key))
             {
@@ -106,15 +106,15 @@ namespace DicitionaryImplementation
         public void  Remove(int key)
         {
             int index = Hash(key);
-            arr_index[index] = -1;
-            empty_position.Add(index);
+            arr_index[index] = -1; // indica que a pos. do array voltou a estar livre
+            empty_position.Add(index); // Adiciona a pos. a lista de pos. vagas
         }
 
         private void Resize()
         {
             primes.RemoveAt(0);
-            size = (int)primes[0];
-            Array.Resize(ref arr_index, size);
+            size = (int)primes[0]; // pega o próximo primo da lista
+            Array.Resize(ref arr_index, size); // Redefine o tamanho do Array baseado no número primo
             Array.Resize(ref arr_items, size);
             
         }
@@ -123,10 +123,10 @@ namespace DicitionaryImplementation
         {
             int index = Hash(key);
              
-            if (empty_position.Contains(index))
+            if (empty_position.Contains(index)) // Verifica se é uma pos livre
                 Console.WriteLine("Item not found");
 
-            else if ((arr_items[index].collision > 0) && key != arr_items[index].key)
+            else if ((arr_items[index].collision > 0) && key != arr_items[index].key) // Se há colisões e a chave n está na pos original então busca na lista next
             {
                 for (int i = 0; i < arr_items[index].next.Count; i++)
                 {
